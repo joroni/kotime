7a6099c997f8f8d26839774c8fd91306983d5421
joroni <jraymund.niconi@gmail.com>
2016-03-27 00:12:34 +0800

    update

--- KO Time/www/app/settings.js
+++ KO Time/www/app/settings.js
@@ -15,7 +15,7 @@ Drupal.settings.debug = true;
 /* DRUPAL PATHS */
 
 // Site Path (do not use a trailing slash)
-Drupal.settings.site_path = 'http://127.0.0.1/DRUPAL7'; // e.g. http://www.example.com
+Drupal.settings.site_path = 'http://localhost/DRUPAL7'; // e.g. http://www.example.com
 
 // Default Services Endpoint Path
 Drupal.settings.endpoint = 'drupalgap';
